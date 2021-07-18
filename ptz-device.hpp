aeced9c48e88e7e8445a09b6498cfd578aa89c17
Grant Likely <grant.likely@secretlab.ca>
2021-07-19 00:21:41 +0100

    Increase number of presets to 16
    VISCA can do up to 16 presets, so create that many.
    
    Fixes: #26
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- ptz-device.hpp
+++ ptz-device.hpp
@@ -29,8 +29,9 @@ public:
 	bool setData(const QModelIndex &index, const QVariant &value, int role);
 };
 
-const QStringList default_preset_names({"Preset 1", "Preset 2", "Preset 3", "Preset 4", "Preset 5",
-					"Preset 6", "Preset 7", "Preset 8", "Preset 9", "Preset 10"});
+const QStringList default_preset_names({"Preset 1", "Preset 2", "Preset 3", "Preset 4",
+	"Preset 5",  "Preset 6",  "Preset 7",  "Preset 8",  "Preset 9",  "Preset 10",
+	"Preset 11", "Preset 12", "Preset 13", "Preset 14", "Preset 15", "Preset 16"});
 
 class PTZDevice : public QObject {
 	Q_OBJECT
