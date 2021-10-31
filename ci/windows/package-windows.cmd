073e4593260de22c85d3480d7d240237095a2c7d
Grant Likely <grant.likely@secretlab.ca>
2021-10-31 14:40:34 +0000

    Use git tag for Windows package version number
    Puts the version number into the Windows zip file and installer
    filenames.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- ci/windows/package-windows.cmd
+++ ci/windows/package-windows.cmd
@@ -3,10 +3,6 @@ call "%~dp0..\ci_includes.generated.cmd"
 mkdir package
 cd package
 
-git rev-parse --short HEAD > package-version.txt
-set /p PackageVersion=<package-version.txt
-del package-version.txt
-
 REM Package ZIP archive
 7z a "%PluginName%-%PackageVersion%-Windows.zip" "..\release\*"
 
