glikely/obs-ptz

commit 8d8b2944b90501b27a9d59a8e06dab4f1c525d55

visca: Add helper for scaling speeds to camera integer
Translating and scaling the pan/tilt/zoom/focus speed from an double
ranged [-1.0,1.0] to an integer that the camera will accept, without
creating an invalid encoding is a little tricky. Create a separate
helper function scale_speed() so that the code can be shared by all the
movement code.

Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

src/protocol-helpers.hpp
@@ -194,3 +194,18 @@ public:
 	{
 	}
 };
+
+/**
+ * scale_speed() - Helper to translate normalized speed to VISCA int
+ * speed: normalized speed in range [-1.0, 1.0]
+ * max: Maximum integer value accepted by camera
+ *
+ * returns an integer value scaled to the range [-max, max]
+ */
+static int scale_speed(double speed, int max)
+{
+	if (speed == 0.0)
+		return 0;
+	return int(std::copysign(
+		std::clamp(abs(speed) * max + 0.5, 1.0, double(max)), speed));
+}

src/ptz-visca.cpp
@@ -816,31 +816,20 @@ void PTZVisca::send_pending()
 	if (pending_cmds.isEmpty()) {
 		if (status & STATUS_PANTILT_SPEED_CHANGED) {
 			status &= ~STATUS_PANTILT_SPEED_CHANGED;
-			int pan = std::clamp(pan_speed, -1.0, 1.0) * 0x18;
-			int tilt = -std::clamp(tilt_speed, -1.0, 1.0) * 0x14;
+			int p = scale_speed(pan_speed, 0x18);
+			int t = scale_speed(tilt_speed, 0x14);
 			PTZCmd cmd = VISCA_PanTilt_drive;
-			cmd.encode({pan, tilt});
+			cmd.encode({p, t});
 			pending_cmds += cmd;
 		} else if (status & STATUS_ZOOM_SPEED_CHANGED) {
 			status &= ~STATUS_ZOOM_SPEED_CHANGED;
-			int speed = std::clamp(zoom_speed, -1.0, 1.0) * 0x7 +
-				    (zoom_speed < 0.0 ? -1 : zoom_speed > 0.0);
 			PTZCmd cmd = VISCA_CAM_Zoom_drive;
-			cmd.encode({speed});
+			cmd.encode({scale_speed(zoom_speed, 0x8)});
 			pending_cmds += cmd;
 		} else if (status & STATUS_FOCUS_SPEED_CHANGED) {
 			status &= ~STATUS_FOCUS_SPEED_CHANGED;
-			// The following two lines allows the focus speed to be
-			// adjusted using the speed slide, but in practical
-			// terms this makes the focus change far too quickly.
-			// Just use the slowest speed instead.
-			//int speed = -(std::clamp(focus_speed, -1.0, 1.0) * 0x7 +
-			//	    (focus_speed < 0.0 ? -1 : focus_speed > 0.0);
-			int speed = -((focus_speed < 0.0)
-					      ? -1
-					      : ((focus_speed > 0.0) ? 1 : 0));
 			PTZCmd cmd = VISCA_CAM_Focus_drive;
-			cmd.encode({speed});
+			cmd.encode({scale_speed(focus_speed, 0x8)});
 			pending_cmds += cmd;
 		} else if (status & STATUS_CONNECTED) {
 			ptz_debug(

commit ad6bfd44db50b9745254337656bf2aa04c27d68a

Move protocol helper code into a cpp file
Protocol helpers was a bit large to just have in a header file. Move all
the method bodies into its own cpp file.

This also fixes a Windows build failure introduced by 8d8b2944, "visca:
Add helper for scaling speeds to camera integer" because the static
function scale_speed() wasn't being used in all users of
protocol-helpers.hpp

The simple constructors are left in the header file because they are
easier to manage that way.

Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

CMakeLists.txt
@@ -45,6 +45,7 @@ target_sources(
           src/ptz-visca.cpp
           src/ptz-visca-udp.cpp
           src/ptz-visca-tcp.cpp
+          src/protocol-helpers.cpp
           src/ptz-action-source.c
           src/imported/qt-wrappers.cpp
           src/imported/obs-app.cpp

src/protocol-helpers.cpp
@@ -1,0 +1,136 @@
+/* PTZ Protocol helper classes and functions
+ *
+ * Copyright 2020-2022 Grant Likely <grant.likely@secretlab.ca>
+ *
+ * SPDX-License-Identifier: GPLv2
+ */
+
+#include <QMap>
+#include "protocol-helpers.hpp"
+
+void bool_field::encode(QByteArray &msg, int val)
+{
+	if (msg.size() < offset + 1)
+		return;
+	msg[offset] = (msg[offset] & ~mask) | (val ? mask : 0);
+}
+
+bool bool_field::decode(OBSData data, QByteArray &msg)
+{
+	if (msg.size() < offset + 1)
+		return false;
+	obs_data_set_bool(data, name, (msg[offset] & mask) != 0);
+	return true;
+}
+
+int_field::int_field(const char *name, unsigned offset, unsigned int mask,
+		     bool signextend)
+	: datagram_field(name, offset), mask(mask)
+{
+	// Calculate number of bytes in the value
+	unsigned int wm = mask;
+	size = 0;
+	while (wm) {
+		wm >>= 8;
+		size++;
+	}
+
+	// Calculate the mask for sign extending
+	if (signextend) {
+		int bitcount = 0;
+		wm = mask;
+		while (wm) {
+			wm &= wm - 1;
+			bitcount++;
+		}
+		extend_mask = 1U << (bitcount - 1);
+	}
+}
+
+void int_field::encode(QByteArray &msg, int val)
+{
+	unsigned int encoded = 0;
+	unsigned int current_bit = 0;
+	unsigned int wm;
+	if (msg.size() < offset + size)
+		return;
+	for (wm = mask; wm; wm = wm >> 1, current_bit++) {
+		if (wm & 1) {
+			encoded |= (val & 1) << current_bit;
+			val = val >> 1;
+		}
+	}
+	wm = mask;
+	for (int i = size - 1; i >= 0; i--) {
+		msg[offset + i] = 0xff & ((~wm & msg[offset + i]) | encoded);
+		wm >>= 8;
+		encoded >>= 8;
+	}
+}
+
+bool int_field::decode_int(int *val_, QByteArray &msg)
+{
+	unsigned int encoded = 0;
+	int val = 0;
+	unsigned int current_bit = 0;
+	if (msg.size() < offset + size)
+		return false;
+	for (int i = 0; i < size; i++)
+		encoded = encoded << 8 | msg[offset + i];
+	for (unsigned int wm = mask; wm; wm >>= 1, encoded >>= 1) {
+		if (wm & 1) {
+			val |= (encoded & 1) << current_bit;
+			current_bit++;
+		}
+	}
+	*val_ = (val ^ extend_mask) - extend_mask;
+	return true;
+}
+
+bool int_field::decode(OBSData data, QByteArray &msg)
+{
+	int val;
+	bool rc = decode_int(&val, msg);
+	if (rc)
+		obs_data_set_int(data, name, val);
+	return rc;
+}
+
+bool string_lookup_field::decode(OBSData data, QByteArray &msg)
+{
+	int val;
+	bool rc = decode_int(&val, msg);
+	if (!rc)
+		return false;
+	obs_data_set_string(data, name, lookup.value(val, "Unknown").c_str());
+	return true;
+}
+
+void PTZCmd::encode(QList<int> arglist)
+{
+	for (int i = 0; i < arglist.size() && i < args.size(); i++)
+		args[i]->encode(cmd, arglist[i]);
+}
+
+obs_data_t *PTZCmd::decode(QByteArray msg)
+{
+	obs_data_t *data = obs_data_create();
+	for (auto field : results)
+		field->decode(data, msg);
+	return data;
+}
+
+/**
+ * scale_speed() - Helper to translate normalized speed to VISCA int
+ * speed: normalized speed in range [-1.0, 1.0]
+ * max: Maximum integer value accepted by camera
+ *
+ * returns an integer value scaled to the range [-max, max]
+ */
+int scale_speed(double speed, int max)
+{
+	if (speed == 0.0)
+		return 0;
+	return int(std::copysign(
+		std::clamp(abs(speed) * max + 0.5, 1.0, double(max)), speed));
+}

src/protocol-helpers.hpp
@@ -1,6 +1,6 @@
-/* Pan Tilt Zoom camera instance
+/* PTZ Protocol helper classes and functions
  *
- * Copyright 2020 Grant Likely <grant.likely@secretlab.ca>
+ * Copyright 2020-2022 Grant Likely <grant.likely@secretlab.ca>
  *
  * SPDX-License-Identifier: GPLv2
  */
@@ -32,19 +32,8 @@ public:
 		: datagram_field(name, offset), mask(mask)
 	{
 	}
-	void encode(QByteArray &msg, int val)
-	{
-		if (msg.size() < offset + 1)
-			return;
-		msg[offset] = (msg[offset] & ~mask) | (val ? mask : 0);
-	}
-	bool decode(OBSData data, QByteArray &msg)
-	{
-		if (msg.size() < offset + 1)
-			return false;
-		obs_data_set_bool(data, name, (msg[offset] & mask) != 0);
-		return true;
-	}
+	void encode(QByteArray &msg, int val);
+	bool decode(OBSData data, QByteArray &msg);
 };
 
 class int_field : public datagram_field {
@@ -52,101 +41,23 @@ public:
 	const unsigned int mask;
 	int size, extend_mask = 0;
 	int_field(const char *name, unsigned offset, unsigned int mask,
-		  bool signextend = false)
-		: datagram_field(name, offset), mask(mask)
-	{
-		// Calculate number of bytes in the value
-		unsigned int wm = mask;
-		size = 0;
-		while (wm) {
-			wm >>= 8;
-			size++;
-		}
-
-		// Calculate the mask for sign extending
-		if (signextend) {
-			int bitcount = 0;
-			wm = mask;
-			while (wm) {
-				wm &= wm - 1;
-				bitcount++;
-			}
-			extend_mask = 1U << (bitcount - 1);
-		}
-	}
-	void encode(QByteArray &msg, int val)
-	{
-		unsigned int encoded = 0;
-		unsigned int current_bit = 0;
-		unsigned int wm;
-		if (msg.size() < offset + size)
-			return;
-		for (wm = mask; wm; wm = wm >> 1, current_bit++) {
-			if (wm & 1) {
-				encoded |= (val & 1) << current_bit;
-				val = val >> 1;
-			}
-		}
-		wm = mask;
-		for (int i = size - 1; i >= 0; i--) {
-			msg[offset + i] = 0xff &
-					  ((~wm & msg[offset + i]) | encoded);
-			wm >>= 8;
-			encoded >>= 8;
-		}
-	}
-
-	bool decode_int(int *val_, QByteArray &msg)
-	{
-		unsigned int encoded = 0;
-		int val = 0;
-		unsigned int current_bit = 0;
-		if (msg.size() < offset + size)
-			return false;
-		for (int i = 0; i < size; i++)
-			encoded = encoded << 8 | msg[offset + i];
-		for (unsigned int wm = mask; wm; wm >>= 1, encoded >>= 1) {
-			if (wm & 1) {
-				val |= (encoded & 1) << current_bit;
-				current_bit++;
-			}
-		}
-		*val_ = (val ^ extend_mask) - extend_mask;
-		return true;
-	}
-
-	bool decode(OBSData data, QByteArray &msg)
-	{
-		int val;
-		bool rc = decode_int(&val, msg);
-		if (rc)
-			obs_data_set_int(data, name, val);
-		return rc;
-	}
+		  bool signextend = false);
+	void encode(QByteArray &msg, int val);
+	bool decode_int(int *val_, QByteArray &msg);
+	bool decode(OBSData data, QByteArray &msg);
 };
 
 class string_lookup_field : public int_field {
 public:
-	const QMap<int, std::string> *lookup;
+	const QMap<int, std::string> &lookup;
 	string_lookup_field(const char *name,
 			    const QMap<int, std::string> &lookuptable,
 			    unsigned offset, unsigned int mask,
 			    bool signextend = false)
-		: int_field(name, offset, mask, signextend)
+		: int_field(name, offset, mask, signextend), lookup(lookuptable)
 	{
-		lookup = &lookuptable;
 	}
-
-	bool decode(OBSData data, QByteArray &msg)
-	{
-		int val;
-		bool rc = decode_int(&val, msg);
-		if (!rc)
-			return false;
-		obs_data_set_string(data, name,
-				    lookup->value(val, "Unknown").c_str());
-		return true;
-	}
+	bool decode(OBSData data, QByteArray &msg);
 };
 
 class PTZCmd {
@@ -171,18 +82,8 @@ public:
 		: cmd(QByteArray::fromHex(cmd_hex)), args(args), results(rslts)
 	{
 	}
-	void encode(QList<int> arglist)
-	{
-		for (int i = 0; i < arglist.size() && i < args.size(); i++)
-			args[i]->encode(cmd, arglist[i]);
-	}
-	obs_data_t *decode(QByteArray msg)
-	{
-		obs_data_t *data = obs_data_create();
-		for (auto field : results)
-			field->decode(data, msg);
-		return data;
-	}
+	void encode(QList<int> arglist);
+	obs_data_t *decode(QByteArray msg);
 };
 
 class PTZInq : public PTZCmd {
@@ -195,17 +96,4 @@ public:
 	}
 };
 
-/**
- * scale_speed() - Helper to translate normalized speed to VISCA int
- * speed: normalized speed in range [-1.0, 1.0]
- * max: Maximum integer value accepted by camera
- *
- * returns an integer value scaled to the range [-max, max]
- */
-static int scale_speed(double speed, int max)
-{
-	if (speed == 0.0)
-		return 0;
-	return int(std::copysign(
-		std::clamp(abs(speed) * max + 0.5, 1.0, double(max)), speed));
-}
+extern int scale_speed(double speed, int max);