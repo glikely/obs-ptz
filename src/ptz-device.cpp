b2930ca17020b6aaf01eba56f466ae2e0aafce09
Grant Likely <grant.likely@secretlab.ca>
2026-06-04 17:12:17 +0100

    Simplify PTZPresetModel::insertRows()
    Replaces direct access to the data and uses a method call to create new
    presets instead.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- src/ptz-device.cpp
+++ src/ptz-device.cpp
@@ -454,8 +454,10 @@ void PTZDevice::setPresetName(size_t id, QString name)
 }
 
 /* Insert a new preset and return the ID */
-int PTZDevice::newPreset()
+int PTZDevice::newPreset(int row)
 {
+	if ((row < 0) || (row > m_presets.size()))
+		row = m_presets.size();
 	size_t id = 0;
 	while (m_presets.contains(id))
 		id++;
@@ -466,7 +468,7 @@ int PTZDevice::newPreset()
 	map["id"] = (uint)id;
 	//FIXME: m_presetsModel.beginInsertRows(QModelIndex(), rowCount(), 1);
 	m_presets[id] = map;
-	m_presetsDisplayOrder.append(id);
+	m_presetsDisplayOrder.insert(row, id);
 	//FIXME: m_presetsModel.endInsertRows();
 	return (int)id;
 }
